14cd341e3a0f85f69c71e9b76e69f4bd7971ac21
Yuriy <yuriy_sh@zoho.com>
2020-07-01 01:40:31 +0300

    feat: add skills

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import skills from '../../data/skills';
+import './Skills.css';
 
 export default function Skills() {
+  const skillsElements = skills.map((element) => (
+    <Col xs={12} md={6} xl={4} className="skills__skill">
+      <div className="skills__skill-icon-block">{element.icon}</div>
+      <div className="skills__skill-description-block">
+        <h2 className="skills__skill-title">{element.title}</h2>
+      </div>
+    </Col>
+  ));
   return (
     <Container>
-      <Row>
-        <Col>
-          <h2>Skills</h2>
-        </Col>
-      </Row>
+      <Row>{skillsElements}</Row>
     </Container>
-  )
+  );
 }
---
+++ src/data/skills.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { FaSass } from 'react-icons/fa';
+import { GrCss3, GrGithub, GrHtml5, GrJs, GrMysql, GrReactjs, GrTechnology, GrTerminal } from 'react-icons/gr';
+import { IconContext } from 'react-icons/lib';
+
+export default [
+  {
+    title: 'React',
+    icon: (
+      <IconContext.Provider value={{ className: 'skills__skill-icon', size: '6.5rem' }}>
+        <GrReactjs />
+      </IconContext.Provider>
+    ),
+  },
+  {
+    title: 'Javascript ES6 syntax',
+    icon: (
+      <IconContext.Provider value={{ className: 'skills__skill-icon', size: '6.5rem' }}>
+        <GrJs />
+      </IconContext.Provider>
+    ),
+  },
+  {
+    title: 'CSS3',
+    icon: (
+      <IconContext.Provider value={{ className: 'skills__skill-icon', size: '6.5rem' }}>
+        <GrCss3 />
+      </IconContext.Provider>
+    ),
+  },
+  {
+    title: 'SASS',
+    icon: (
+      <IconContext.Provider value={{ className: 'skills__skill-icon', size: '6.5rem' }}>
+        <FaSass />
+      </IconContext.Provider>
+    ),
+  },
+  {
+    title: 'HTML5',
+    icon: (
+      <IconContext.Provider value={{ className: 'skills__skill-icon', size: '6.5rem' }}>
+        <GrHtml5 />
+      </IconContext.Provider>
+    ),
+  },
+  {
+    title: 'WEBPACK',
+    icon: (
+      <IconContext.Provider value={{ className: 'skills__skill-icon', size: '6.5rem' }}>
+        <GrTechnology />
+      </IconContext.Provider>
+    ),
+  },
+  {
+    title: 'GitHub',
+    icon: (
+      <IconContext.Provider value={{ className: 'skills__skill-icon', size: '6.5rem' }}>
+        <GrGithub />
+      </IconContext.Provider>
+    ),
+  },
+  {
+    title: 'Bash',
+    icon: (
+      <IconContext.Provider value={{ className: 'skills__skill-icon', size: '6.5rem' }}>
+        <GrTerminal />
+      </IconContext.Provider>
+    ),
+  },
+  {
+    title: 'Mysql',
+    icon: (
+      <IconContext.Provider value={{ className: 'skills__skill-icon', size: '6.5rem' }}>
+        <GrMysql />
+      </IconContext.Provider>
+    ),
+  },
+];
