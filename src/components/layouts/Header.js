4f6a445c8c4031166bed0cb858249cf34d02cf9d
Yuriy <yuriy_sh@zoho.com>
2020-07-24 13:18:22 +0300

    remove target __blank and evenkey properties

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -3,7 +3,6 @@ import { Button, Col, Container, Nav, Row } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
 
-const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
 const Header = () => (
   <header className="header">
     <Container>
@@ -12,18 +11,18 @@ const Header = () => (
           <h1 className="header__title">YURIY SHILENKOV</h1>
         </Col>
         <Col xs={12} md={"auto"} className="header__column-2">
-          <Nav className="header__navbar" variant="tabs" activeKey="1" onSelect={handleSelect}>
-            <Nav.Item eventkey="1" href="/">
+          <Nav className="header__navbar" variant="tabs" activeKey="1">
+            <Nav.Item>
               <Nav.Link className="header__nav-link" as={NavLink} exact to="/">
                 Works List
               </Nav.Link>
             </Nav.Item>
-            <Nav.Item eventkey="2" href="/skills">
+            <Nav.Item>
               <Nav.Link className="header__nav-link" as={NavLink} to="/skills">
                 Skills
               </Nav.Link>
             </Nav.Item>
-            <Nav.Item eventkey="3" href="/contacts">
+            <Nav.Item>
               <Nav.Link className="header__nav-link" as={NavLink} to="/contacts">
                 Contacts
               </Nav.Link>
--- src/components/worksList/Work.js
+++ src/components/worksList/Work.js
@@ -6,11 +6,17 @@ import './Work.css';
 
 export default function Work(props) {
   const { url, title, description, img, githubUrl } = props.project;
+  const handleClick = (e) => {
+    e.preventDefault();
+    window.open(e.currentTarget.href);
+    return false;
+  };
   let githubLink = '';
+
   if (githubUrl) {
     githubLink = (
       <IconContext.Provider value={{ className: ' works-list__card-github-icon', size: '2.5rem' }}>
-        <Card.Link href={githubUrl} className="works-list__card-link" target="__blank">
+        <Card.Link href={githubUrl} className="works-list__card-link"  onClick={handleClick}>
           <FaGithubSquare />
         </Card.Link>
       </IconContext.Provider>
@@ -20,12 +26,12 @@ export default function Work(props) {
   return (
     <Col className="works-list__column" xs={12} md={6} xl={4}>
       <Card border="info" className="works-list__card">
-        <Card.Img variant="top" src={img} />
+        <Card.Img variant="top" src={img} alt={`${title} image`}/>
         <Card.Body>
           <Card.Title className="works-list__card-title">{title}</Card.Title>
           <Card.Text className="works-list__card-description">{description}</Card.Text>
           <div className="works-list__card-buttons-block">
-            <Button className="works-list__card-button" href={url} variant="primary" target="__blank">
+            <Button className="works-list__card-button" href={url} variant="primary" onClick={handleClick}>
               Go to site
             </Button>{' '}
             {githubLink}
